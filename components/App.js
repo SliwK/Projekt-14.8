9f3e071addc147b86ed64523c9afb977764e3d06
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-01-24 23:21:37 +0100

    Correct js

--- components/App.js
+++ components/App.js
@@ -50,7 +50,7 @@ App = React.createClass({
         return (
           <div style={styles}>
                 <h1>Wyszukiwarka GIFow!</h1>
-                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+                <p>Znajdź gifa na <a href='https://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
                 <Search onSearch={this.handleSearch}/>
             <Gif
                 loading={this.state.loading}
