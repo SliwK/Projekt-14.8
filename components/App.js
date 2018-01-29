a0549488c21357a0f539d91e58ce927d288b0e72
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-01-30 00:12:21 +0100

    Correct js

--- components/App.js
+++ components/App.js
@@ -22,7 +22,7 @@ App = React.createClass({
     },
 
     getGif: function(searchingText, callback) {
-        var GIPHY_API_URL = 'http://api.giphy.com';
+        var GIPHY_API_URL = 'https://api.giphy.com';
         var GIPHY_PUB_KEY = "dc6zaTOxFJmzC";
         var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
         var xhr = new XMLHttpRequest();
@@ -50,7 +50,7 @@ App = React.createClass({
         return (
           <div style={styles}>
                 <h1>Wyszukiwarka GIFow!</h1>
-                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+                <p>Znajdź gifa na <a href='https://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
                 <Search onSearch={this.handleSearch}/>
             <Gif
                 loading={this.state.loading}
